0457c45e7c592deeafd7c96a65fdd6cd572205d3
Savas Aydin <savasaydin@gmail.com>
2017-11-16 12:44:46 +0100

    Converts 1 to "1st"

--- ordinal_numbers/test/ordinal_numbers_tests.erl
+++ ordinal_numbers/test/ordinal_numbers_tests.erl
@@ -4,3 +4,7 @@
 convert_0_to_0_test() ->
     Res = ordinal_numbers:to_ordinal(0),
     ?assertEqual("0", Res).
+
+convert_1_to_1st_test() ->
+    Res = ordinal_numbers:to_ordinal(1),
+    ?assertEqual("1st", Res).
