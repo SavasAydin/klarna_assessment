34887ea8544112b1b34561ea3be844bdde1637d1
Savas Aydin <savasaydin@gmail.com>
2017-11-16 12:32:59 +0100

    Verifies that it masks numbers of an unusual credit card

--- credit_card/test/credit_card_tests.erl
+++ credit_card/test/credit_card_tests.erl
@@ -15,3 +15,8 @@ should_mask_numbers_of_standard_credit_cards_test() ->
     CreditCard = "4556364607935616",
     Res = credit_card:maskify(CreditCard),
     ?assertEqual("4###########5616", Res).
+
+should_mask_numbers_of_unusual_credit_cards_test() ->
+    CreditCard = "64607935616",
+    Res = credit_card:maskify(CreditCard),
+    ?assertEqual("6######5616", Res).
