982840fb84b77a486ed7db764ab5e52d70b5530c
Savas Aydin <savasaydin@gmail.com>
2017-11-16 13:35:02 +0100

    Checks if the expression contains any operations

--- calculator/src/calculator.erl
+++ calculator/src/calculator.erl
@@ -5,10 +5,17 @@ calculate("") ->
     0;
 calculate(Expression) ->
     Tokens = string:tokens(Expression, " "),
-    last(Tokens).
+    case contains_operation(Tokens) of
+	false ->
+	    last(Tokens)
+    end.
 
 last(Tokens) ->
     to_float(lists:last(Tokens)).
 
 to_float(Str) ->
     float(list_to_integer(Str)).
+
+contains_operation(L) ->
+    Operations = ["+", "-", "*", "/"],
+    lists:any(fun(X) -> lists:member(X, Operations) end, L).
