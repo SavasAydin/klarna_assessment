6e2d0f7ad899d17a3266d93877569e69ffce42cd
Savas Aydin <savas.aydin@ericsson.com>
2018-10-28 20:53:06 +0100

    Done from scratch second time.

--- credit_card/src/credit_card.erl
+++ credit_card/src/credit_card.erl
@@ -1,14 +1,14 @@
 -module(credit_card).
 -export([maskify/1]).
 
-maskify(CreditCard) when length(CreditCard) < 6 ->
-    CreditCard;
-maskify([H|CreditCard]) ->
-    SplitAt = length(CreditCard) - 4,
-    {ToMask, NotToMask} = lists:split(SplitAt, CreditCard),
-    [H | [ mask_digits(X) || X <- ToMask ] ++ NotToMask].
+maskify([]) ->
+    [];
+maskify([N|Numbers]) ->
+    SplitAt = length(Numbers) - 4,
+    {ToMask, NotToMask} = lists:split(SplitAt, Numbers),
+    [N | lists:map(fun mask_digits/1, ToMask)] ++ NotToMask.
 
-mask_digits(D) when D >= $0 andalso D =< $9 ->
+mask_digits(N) when N >= $0 andalso N =<$9 ->
     $#;
-mask_digits(NonDigit) ->
-    NonDigit.
+mask_digits(NotADigit) ->
+    NotADigit.
--- credit_card/test/credit_card_tests.erl
+++ credit_card/test/credit_card_tests.erl
@@ -1,32 +1,22 @@
 -module(credit_card_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-should_not_mask_empty_credit_cards_test() ->
-    CreditCard = "",
-    Res = credit_card:maskify(CreditCard),
+should_not_mask_if_no_numbers_given_test() ->
+    Res = credit_card:maskify(""),
     ?assertEqual("", Res).
 
-should_not_mask_numbers_of_short_credit_cards_test() ->
-    CreditCard = "12345",
-    Res = credit_card:maskify(CreditCard),
-    ?assertEqual(CreditCard, Res).
+should_not_mask_if_number_is_five_digits_test() ->
+    Res = credit_card:maskify("12345"),
+    ?assertEqual("12345", Res).
 
-should_mask_numbers_of_standard_credit_cards_test() ->
-    CreditCard = "4556364607935616",
-    Res = credit_card:maskify(CreditCard),
+should_mask_all_but_first_and_last_four_digits_test() ->
+    Res = credit_card:maskify("4556364607935616"),
     ?assertEqual("4###########5616", Res).
 
-should_mask_numbers_of_unusual_credit_cards_test() ->
-    CreditCard = "64607935616",
-    Res = credit_card:maskify(CreditCard),
-    ?assertEqual("6######5616", Res).
-
-should_not_mask_non_digits_of_standard_credit_cards_test() ->
-    CreditCard = "4556-3646-0793-5616",
-    Res = credit_card:maskify(CreditCard),
+should_mask_all_but_hyphen_first_and_last_four_digits_test() ->
+    Res = credit_card:maskify("4556-3646-0793-5616"),
     ?assertEqual("4###-####-####-5616", Res).
 
-should_not_mask_non_credit_cards_test() ->
-    CreditCard = "Not a credit card",
-    Res = credit_card:maskify(CreditCard),
-    ?assertEqual(CreditCard, Res).
+should_not_mask_if_numbers_are_not_integer_test() ->
+    Res = credit_card:maskify("Skippy"),
+    ?assertEqual("Skippy", Res).
