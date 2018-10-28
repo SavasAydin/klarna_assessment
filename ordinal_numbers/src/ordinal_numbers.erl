e80bd00f8d84019204470bcd2ca5e3042c92a21b
Savas Aydin <savas.aydin@ericsson.com>
2018-10-28 20:15:01 +0100

    Done from scratch second time.

--- ordinal_numbers/src/ordinal_numbers.erl
+++ ordinal_numbers/src/ordinal_numbers.erl
@@ -1,15 +1,17 @@
 -module(ordinal_numbers).
--export([to_ordinal/1]).
+-export([numberToOrdinal/1]).
 
--define(SUFFIXES, [{1, "st"}, {2, "nd"}, {3, "rd"}]).
--define(DEFAULT_SUFFIX, "th").
-
-to_ordinal(0) ->
-    "0";
-to_ordinal(N) ->
+numberToOrdinal(0) ->
+    0;
+numberToOrdinal(N) ->
     Suffix = get_suffix(N),
     integer_to_list(N) ++ Suffix.
 
-get_suffix(N) ->
-    Ending = N rem 10,
-    proplists:get_value(Ending, ?SUFFIXES, ?DEFAULT_SUFFIX).
+get_suffix(N) when N rem 10 == 1 andalso N /= 11 ->
+    "st";
+get_suffix(N) when N rem 10 == 2 andalso N /= 12 ->
+    "nd";
+get_suffix(N) when N rem 10 == 3 andalso N /= 13 ->
+    "rd";
+get_suffix(_) ->
+    "th".
--- ordinal_numbers/test/ordinal_numbers_tests.erl
+++ ordinal_numbers/test/ordinal_numbers_tests.erl
@@ -1,46 +1,51 @@
 -module(ordinal_numbers_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-convert_0_to_0_test() ->
-    Res = ordinal_numbers:to_ordinal(0),
-    ?assertEqual("0", Res).
+convert_0_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(0),
+    ?assertEqual(0, Res).
 
-convert_1_to_1st_test() ->
-    Res = ordinal_numbers:to_ordinal(1),
+convert_1_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(1),
     ?assertEqual("1st", Res).
 
-convert_2_to_2st_test() ->
-    Res = ordinal_numbers:to_ordinal(2),
+convert_2_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(2),
     ?assertEqual("2nd", Res).
 
-convert_3_to_3rd_test() ->
-    Res = ordinal_numbers:to_ordinal(3),
+convert_3_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(3),
     ?assertEqual("3rd", Res).
 
-convert_4_to_4th_test() ->
-    Res = ordinal_numbers:to_ordinal(4),
+convert_4_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(4),
     ?assertEqual("4th", Res).
 
-convert_15_to_15th_test() ->
-    Res = ordinal_numbers:to_ordinal(15),
-    ?assertEqual("15th", Res).
-
-convert_21_to_21st_test() ->
-    Res = ordinal_numbers:to_ordinal(21),
+convert_21_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(21),
     ?assertEqual("21st", Res).
 
-convert_32_to_32nd_test() ->
-    Res = ordinal_numbers:to_ordinal(32),
+convert_32_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(32),
     ?assertEqual("32nd", Res).
 
-convert_43_to_43rd_test() ->
-    Res = ordinal_numbers:to_ordinal(43),
+convert_43_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(43),
     ?assertEqual("43rd", Res).
 
-convert_100_to_100th_test() ->
-    Res = ordinal_numbers:to_ordinal(100),
-    ?assertEqual("100th", Res).
+convert_99_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(99),
+    ?assertEqual("99th", Res).
 
-convert_10000_to_10000th_test() ->
-    Res = ordinal_numbers:to_ordinal(10000),
-    ?assertEqual("10000th", Res).
+convert_11_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(11),
+    ?assertEqual("11th", Res).
+
+convert_12_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(12),
+    ?assertEqual("12th", Res).
+
+convert_13_to_ordinal_number_test() ->
+    Res = ordinal_numbers:numberToOrdinal(13),
+    ?assertEqual("13th", Res).
+
