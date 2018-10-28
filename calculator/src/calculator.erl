c128440917637ec7a13ac88d5b6abb7079b9a799
Savas Aydin <savas.aydin@ericsson.com>
2018-10-28 19:44:10 +0100

    Done from scratch again.

--- calculator/src/calculator.erl
+++ calculator/src/calculator.erl
@@ -1,47 +1,32 @@
 -module(calculator).
--export([calculate/1]).
+-export([evaluate/1]).
 
--define(OPERATIONS, ["+", "-", "*", "/"]).
+-define(OPERATORS, ["+", "-", "*", "/"]).
 
-calculate("") ->
-    0;
-calculate(Expression) ->
+evaluate(Expression) ->
     Tokens = string:tokens(Expression, " "),
-    case contains_operation(Tokens) of
-	false ->
-	    last(Tokens);
-	true ->
-	    calculate(Tokens, [])
+    calculate(Tokens, []).
+
+calculate([], Res) ->
+    hd(Res);
+calculate([H | T], Numbers) ->
+    case lists:member(H, ?OPERATORS) of
+        true ->
+            Applied = apply_operator(H, Numbers),
+            calculate(T, Applied);
+        false ->
+            Number = list_to_number(H),
+            calculate(T, [Number | Numbers])
     end.
 
-last(Tokens) ->
-    to_float(lists:last(Tokens)).
+apply_operator(Operator, [X, Y | T]) ->
+    [apply(erlang, list_to_atom(Operator), [Y,X]) | T].
 
-
-calculate([], [Res]) ->
-    Res;
-calculate([H| T], Acc) ->
-    NewAcc = evaluate(H, Acc),
-    calculate(T, NewAcc).
-
-evaluate("+", [Last, Previous | T]) ->
-    [Previous + Last | T];
-evaluate("*", [Last, Previous | T]) ->
-    [Previous * Last | T];
-evaluate("-", [Last, Previous | T]) ->
-    [Previous - Last | T];
-evaluate("/", [Last, Previous | T]) ->
-    [Previous / Last | T];
-evaluate(X, L) ->
-    [to_float(X) | L].
-
-to_float(Str) ->
-    case lists:member($., Str) of
-	true ->
-	    list_to_float(Str);
-	false ->
-	    float(list_to_integer(Str))
+list_to_number(Num) ->
+    case lists:member($., Num) of
+        true ->
+            list_to_float(Num);
+        false ->
+            list_to_integer(Num)
     end.
 
-contains_operation(L) ->
-    lists:any(fun(X) -> lists:member(X, ?OPERATIONS) end, L).
--- calculator/test/calculator_tests.erl
+++ calculator/test/calculator_tests.erl
@@ -1,38 +1,42 @@
 -module(calculator_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-should_evaluate_empty_expression_to_zero_test() ->
-    Res = calculator:calculate(""),
-    ?assertEqual(0, Res).
-
-should_return_last_number_if_no_operations_found_test() ->
-    Res = calculator:calculate("1 2 3"),
-    ?assertEqual(3.0, Res).
-
-should_return_last_float_if_no_operations_found_test() ->
-    Res = calculator:calculate("1 2 3.5"),
-    ?assertEqual(3.5, Res).
+numbers_without_operator_returns_last_value_test() ->
+    Expression = "2 1 3",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(3, Res).
 
 should_support_addition_test() ->
-    Res = calculator:calculate("1 2 +"),
-    ?assertEqual(3.0, Res).
-
-should_support_multiplication_test() ->
-    Res = calculator:calculate("1 2 *"),
-    ?assertEqual(2.0, Res).
+    Expression = "2 1 +",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(3, Res).
 
 should_support_subtraction_test() ->
-    Res = calculator:calculate("1 2 -"),
-    ?assertEqual(-1.0, Res).
+    Expression = "2 1 -",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(1, Res).
+
+should_support_multiplication_test() ->
+    Expression = "3 2 *",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(6, Res).
 
 should_support_division_test() ->
-    Res = calculator:calculate("4 2 /"),
+    Expression = "4 2 /",
+    Res = calculator:evaluate(Expression),
     ?assertEqual(2.0, Res).
 
-should_support_more_than_one_operation_test() ->
-    Res = calculator:calculate("3 2 1 - *"),
-    ?assertEqual(3.0, Res).
+should_support_floating_numbers_test() ->
+    Expression = "3.0 2.0 +",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(5.0, Res).
 
-should_support_all_operations_test() ->
-    Res = calculator:calculate("7 4 5 + * 3 - 10 /"),
+should_support_multiple_operators_in_order_test() ->
+    Expression = "3 2 1 - *",
+    Res = calculator:evaluate(Expression),
+    ?assertEqual(3, Res).
+
+should_support_multiple_operators_in_mix_order_test() ->
+    Expression = "7 4 5 + * 3 - 10 /",
+    Res = calculator:evaluate(Expression),
     ?assertEqual(6.0, Res).
